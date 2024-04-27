77349579f37c6a2b3e8fc7d74a43ac05bd30d0c1
Julien <gardier.julien@gmail.com>
2024-04-27 12:02:24 +0200

    moderateImage middleware added in POST and PUT routes/library.js

---
+++ backend/middlewares/moderation.js
@@ -0,0 +1,26 @@
+const { default: axios } = require("axios");
+require("dotenv").config({ path: "backend/.env" });
+
+const moderateImage = async (req, res, next) => {
+    const imageUrl = req.body.imageUrl;
+    const apiKey = process.env.KEY_API_MODERATIONCONTENT;
+
+    try {
+        const response = await axios.get(
+            `https://api.moderationcontent.com/moderate?url=${imageUrl}&key=${apiKey}`
+        );
+        // accolades pour destructurer la réponse de l'API et récupérer les prédictions comme
+        // indiqué dans la documentation de l'API
+        const { predictions } = response.data;
+
+        // Si la prédiction est supérieure à 50%, on renvoie une erreur 400 avec un message d'erreur
+        if (predictions.adult > 50) {
+            return res.status(400).json({ error: "Image non autorisée" });
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+};
+
+module.exports = moderateImage;
--- backend/routes/library.js
+++ backend/routes/library.js
@@ -4,13 +4,14 @@ const auth = require('../middlewares/auth');
 const { upload, processImage } = require('../middlewares/multer-config');
 const bookCtrl = require('../controllers/library');
 const uploadToCloudinary = require('../middlewares/cloudinary');
+const moderateImage = require('../middlewares/moderation');
 
 
 router.get('/', bookCtrl.getAllBooks);
-router.post('/', auth, upload, processImage, uploadToCloudinary, bookCtrl.createBook);
+router.post('/', auth, upload, processImage, moderateImage, uploadToCloudinary, bookCtrl.createBook);
 router.get('/bestrating', bookCtrl.bestRating);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', auth, upload, processImage, uploadToCloudinary, bookCtrl.modifyBook);
+router.put('/:id', auth, upload, processImage, moderateImage, uploadToCloudinary, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.createRating);
 
