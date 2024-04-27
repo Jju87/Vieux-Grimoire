3b2e56bc07c74917bebb4e6a67399c3a677ab7b6
Julien <gardier.julien@gmail.com>
2024-04-28 01:20:34 +0200

    middleare logs

--- backend/middlewares/moderation.js
+++ backend/middlewares/moderation.js
@@ -4,23 +4,26 @@ require("dotenv").config({ path: "backend/.env" });
 const moderateImage = async (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const apiKey = process.env.KEY_API_MODERATIONCONTENT;
-    // console.log("imageUrl in moderateImage: ", imageUrl);
-    // console.log("apiKey in moderateImage: ", apiKey);
 
     try {
         const response = await axios.get(
             `https://api.moderatecontent.com/moderate/?url=${imageUrl}&key=${apiKey}`
-          );
-        // accolades pour destructurer la réponse de l'API et récupérer les prédictions comme
-        // indiqué dans la documentation de l'API
+        );
+
+        console.log("Response from ModerateContent API: ", response.data); // Log the complete response
+
         const { predictions } = response.data;
-        console.log("Adult score: ", predictions.adult); // Log adult score
 
+        if (predictions && predictions.adult !== undefined) {
+            console.log("Adult score: ", predictions.adult); // Log adult score
 
-        // Si la prédiction est supérieure à 50%, on renvoie une erreur 400 avec un message d'erreur
-        if (predictions.adult > 50) {
-            return res.status(400).json({ error: "Image non autorisée" });
+            if (predictions.adult > 50) {
+                return res.status(400).json({ error: "Image non autorisée" });
+            }
+        } else {
+            console.log("No adult score returned from ModerateContent API");
         }
+
         next();
     } catch (error) {
         res.status(500).json({ error });
