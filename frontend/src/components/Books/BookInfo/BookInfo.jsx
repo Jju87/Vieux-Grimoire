261ac8fa32becb06bb3c77bc3e5e4ca6843b8e4d
Julien <gardier.julien@gmail.com>
2024-04-30 22:04:42 +0200

    summary added to bookInfo and bookForm

--- frontend/src/components/Books/BookInfo/BookInfo.jsx
+++ frontend/src/components/Books/BookInfo/BookInfo.jsx
@@ -1,21 +1,25 @@
 import * as PropTypes from 'prop-types';
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from '../../../pages/Book/Book.module.css';
 import { displayStars } from '../../../lib/functions';
 
 function BookInfo({ book }) {
   return (
-    <div className={styles.BookInfo}>
-      <h1>{book.title}</h1>
-      <p className={styles.Author}>{`Auteur: ${book.author}`}</p>
-      <p className={styles.PublishDate}>{book.year}</p>
-      <p className={styles.Genre}>{book.genre}</p>
-      <div className={styles.Rating}>
-        <div>{displayStars(book.averageRating)}</div>
-        <p>{`${book.averageRating}/5`}</p>
+    <>
+      <div className={styles.BookInfo}>
+        <h1>{book.title}</h1>
+        <p className={styles.Author}>{`Auteur: ${book.author}`}</p>
+        <p className={styles.PublishDate}>{book.year}</p>
+        <p className={styles.Genre}>{book.genre}</p>
+        <div className={styles.Rating}>
+          <div>{displayStars(book.averageRating)}</div>
+          <p>{`${book.averageRating}/5`}</p>
+        </div>
       </div>
-      <p className={styles.Summary}>{book.summary}</p>
-    </div>
+      <div className="summaryContainer">
+        <p className={styles.Summary}>{book.summary}</p>
+      </div>
+    </>
   );
 }
 
